e306b73d0fcf9e5ed3db6e943f354cd96c4b398a
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2022-08-13 02:47:04 -0500

    Ajuste de cuchillos y Colisión perder del jugador

--- Dodge/Assets/Scripts/Player_Collisions.cs
+++ Dodge/Assets/Scripts/Player_Collisions.cs
@@ -1,20 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player_Collisions : MonoBehaviour
 {
-    [SerializeField] int newVariable;
-
-
-    void Start()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    void Update()
-    {
-        
+        if(collision.gameObject.CompareTag("Knife"))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
 }
