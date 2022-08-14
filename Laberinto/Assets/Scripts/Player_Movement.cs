a129fb5809dc691197afa15ddc16f2a344795358
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2022-08-14 00:36:03 -0500

    Subir proyecto de Unity con 3d y 2d

---
+++ Laberinto/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] int newVariable;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
