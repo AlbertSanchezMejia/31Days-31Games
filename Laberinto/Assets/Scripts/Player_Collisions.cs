2c21e6e3e8791ca56cdd70838d42ee75478c5739
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2022-08-14 01:58:54 -0500

    Jugador movimiento y colisiones, Posición de la cámara

---
+++ Laberinto/Assets/Scripts/Player_Collisions.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Collisions : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Teleporter"))
+        {
+            Debug.Log(other.gameObject);
+        }
+    }
+
+
+}
--- Laberinto/Assets/Scripts/Player_Movement.cs
+++ Laberinto/Assets/Scripts/Player_Movement.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    [SerializeField] int newVariable;
-
+    [SerializeField] float moveDis;
+    Rigidbody rb;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            MovePosition(Vector3.forward * moveDis);
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            MovePosition(-Vector3.forward * moveDis);
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            MovePosition(Vector3.right * moveDis);
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            MovePosition(-Vector3.right * moveDis);
+        }
+    }
+
+    void MovePosition(Vector3 direction)
+    {
+        rb.MovePosition(transform.position + direction);
     }
 
 }
