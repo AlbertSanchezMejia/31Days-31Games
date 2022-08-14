92dffe25b85fcca16614f1e1fd934f888b3bd00d
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2022-08-14 12:11:11 -0500

    Luces, mecánica de teletransportación

--- Laberinto/Assets/Scripts/Player_Collisions.cs
+++ Laberinto/Assets/Scripts/Player_Collisions.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Player_Collisions : MonoBehaviour
 {
+    bool isTeleporting;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Teleporter"))
         {
-            Debug.Log(other.gameObject);
+            if(isTeleporting)
+            {
+                isTeleporting = false;
+                return;
+            }
+            transform.position = other.gameObject.GetComponent<Teleporter_>().destination.position;
+            isTeleporting = true;
         }
     }
 
