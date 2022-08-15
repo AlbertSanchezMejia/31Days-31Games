fbe7d905c007f3cea21c3ad0aa2147d32f6f59c6
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2022-08-15 12:57:37 -0500

    Genial, el personaje ya no camino fuera del camino
    Fue más fácil de lo que pense xD

--- Laberinto/Assets/Scripts/Player_Movement.cs
+++ Laberinto/Assets/Scripts/Player_Movement.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     [SerializeField] float moveDis;
-    [SerializeField] float speed;
     Rigidbody rb;
-    float xMov;
-    float zMov;
 
     void Start()
     {
@@ -17,33 +14,36 @@ public class Player_Movement : MonoBehaviour
 
     void Update()
     {
-        /*
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
-            MovePosition(Vector3.forward * moveDis);
+            MoveObjectPosition(Vector3.forward * moveDis);
             return;
         }
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
-            MovePosition(-Vector3.forward * moveDis);
+            MoveObjectPosition(-Vector3.forward * moveDis);
             return;
         }
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
-            MovePosition(Vector3.right * moveDis);
+            MoveObjectPosition(Vector3.right * moveDis);
             return;
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
-            MovePosition(-Vector3.right * moveDis);
-        }*/
-        xMov = Input.GetAxisRaw("Horizontal");
-        zMov = Input.GetAxisRaw("Vertical");
+            MoveObjectPosition(-Vector3.right * moveDis);
+        }
     }
 
-    void MovePosition(Vector3 direction)
+
+    void MoveObjectPosition(Vector3 direction)
     {
-        rb.velocity = (new Vector3(xMov, rb.velocity.y, zMov) * speed * Time.fixedDeltaTime * 10);
+        bool canMove = Physics.Raycast(transform.position + direction, -transform.up, 1f);
+        if (canMove == false)
+        {
+            return;
+        }
+        rb.MovePosition(transform.position + direction);
     }
 
 }
