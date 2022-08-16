fa16049168a6d7361280302ec96bf16948704768
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2022-08-16 17:28:27 -0500

    Reducir código a 10 líneas

--- 10LineGames/Assets/Arkanoid/Ball.cs
+++ 10LineGames/Assets/Arkanoid/Ball.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] float startSpeed = 4f;
-    [SerializeField] Rigidbody2D rb;
-    void Start()
-    {
-        Launch();
-    }
-    void Launch()
-    {
-        float xSpeed = Random.Range(0, 2) == 0 ? -1 : 1;
-        float ySpeed = Random.Range(0, 2) == 0 ? -1 : 1;
-        rb.velocity = new Vector2(1, ySpeed) * startSpeed;
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Paddle"))
-        {
-            rb.velocity *= 1.05f;
-        }
-    }
+
 }
--- 10LineGames/Assets/Arkanoid/Gameplay.cs
+++ 10LineGames/Assets/Arkanoid/Gameplay.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class Gameplay : MonoBehaviour
-{
-    [SerializeField] Rigidbody2D rb;
-    [SerializeField] float speed;
+public class Gameplay : MonoBehaviour {
+    [SerializeField] Rigidbody2D playerRb;
+    [SerializeField] Rigidbody2D ballRb;
     float yMov;
-    void Update()
-    {
-        yMov = Input.GetAxisRaw("Vertical");
-    }
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, yMov * speed * Time.fixedDeltaTime * 10);
-    }
+    void Update() { yMov = Input.GetAxisRaw("Vertical"); }
+    private void FixedUpdate() { playerRb.velocity = new Vector2(playerRb.velocity.x, yMov * 35f * Time.fixedDeltaTime * 10); }
+    void Start() { ballRb.velocity = new Vector2(1, (Random.Range(0, 2) == 0 ? -1 : 1)) * 6f; }
+    void OnCollisionEnter2D(Collision2D col) { if (col.gameObject.CompareTag("Paddle")) ballRb.velocity *= 1.1f; }
 }
