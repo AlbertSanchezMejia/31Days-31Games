54979461709ec901364e3e37828db3d092f3fb02
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2022-08-16 20:47:19 -0500

    Avances menu inicial

--- 10LineGames/Assets/Arkanoid/Gameplay.cs
+++ 10LineGames/Assets/Arkanoid/Gameplay.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 public class Gameplay : MonoBehaviour {
     [SerializeField] Rigidbody2D playerRb;
-    [SerializeField] Rigidbody2D ballRb;
     float yMov;
     void Update() { yMov = Input.GetAxisRaw("Vertical"); }
     private void FixedUpdate() { playerRb.velocity = new Vector2(playerRb.velocity.x, yMov * 35f * Time.fixedDeltaTime * 10); }
+    [SerializeField] Rigidbody2D ballRb;
     void Start() { ballRb.velocity = new Vector2(1, (Random.Range(0, 2) == 0 ? -1 : 1)) * 6f; }
     void OnCollisionEnter2D(Collision2D col) { if (col.gameObject.CompareTag("Paddle")) ballRb.velocity *= 1.1f; }
 }
